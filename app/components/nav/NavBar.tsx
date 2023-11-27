61a49a2799b33eb84c3a2a7eadd772da0927c901
Hoh1996 <wenchun.sitegiant@gmail.com>
2023-11-27 20:36:52 +0800

    Add navbar

--- app/components/nav/NavBar.tsx
+++ app/components/nav/NavBar.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import Container from "./Container";
+import Link from "next/link";
+import { Redressed } from "next/font/google";
+
+const redressed = Redressed({ subsets: ["latin"], weight: ["400"] });
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <div
+      className="sticky top-0 w-full
+  bg-slate-200 z-30 shadow-sm"
+    >
+      <div className="py-4 border-b-[1px]">
+        <Container>
+          <div
+            className="flex items-center justify-between gap-3
+          md:gap-0"
+          >
+            <Link
+              className={`${redressed.className} font-bold text-2xl`}
+              href="/"
+            >
+              E-Shop
+            </Link>
+            <div className="hidden md:block">Search</div>
+            <div className="flex items-center gap-8 md:gap-12">
+              <div>CartCount</div>
+              <div>userMenu</div>
+            </div>
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
+};
 
-export default NavBar
+export default NavBar;
