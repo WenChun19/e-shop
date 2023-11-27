a99b476741c0f8ff7863807f700664fd2c50e29e
Hoh1996 <wenchun.sitegiant@gmail.com>
2023-11-27 21:16:26 +0800

    Add product cards

---
+++ app/components/products/ProductCard.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { formatPrice } from "@/utils/formatPrice";
+import { truncateText } from "@/utils/truncateText";
+import { Rating } from "@mui/material";
+import Image from "next/image";
+import React from "react";
+
+interface ProductCardProps {
+  data: any;
+}
+
+const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
+  const productRating =
+    data.reviews.reduce((acc: number, item: any) => {
+      return item.rating + acc;
+    }, 0) / data.reviews.length;
+
+  return (
+    <div
+      className="col-span-1 cursor-pointer border-[1.2px]
+    border-slate-200 bg-slate-50 rounded-sm
+    p-2 transition hover:scale-105 text-center
+    text-sm"
+    >
+      <div
+        className="flex flex-col items-center
+      w-full gap-1"
+      >
+        <div className="aspect-square relative overflow-hidden w-full">
+          <Image
+            fill
+            className="w-full h-full object-contain"
+            src={data.images[0].image}
+            alt={data.name}
+          />
+        </div>
+        <div className="mt-4">{truncateText(data.name)}</div>
+        <div>
+          <Rating value={productRating} readOnly />
+        </div>
+        <div>{data.reviews.length} reviews</div>
+        <div className="font-semibold">{formatPrice(data.price)}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
+import { products } from "@/utils/products";
 import HomeBanner from "./components/HomeBanner";
 import Container from "./components/nav/Container";
+import { truncateText } from "@/utils/truncateText";
+import ProductCard from "./components/products/ProductCard";
 
 export default function Home() {
   return (
@@ -7,6 +10,15 @@ export default function Home() {
       <Container>
         <div>
           <HomeBanner />
+        </div>
+        <div
+          className="grid grid-cols-2 sm:grid-cols-3
+        lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6
+        gap-8"
+        >
+          {products.map((product: any) => {
+            return <ProductCard data={product} />;
+          })}
         </div>
       </Container>
     </div>
