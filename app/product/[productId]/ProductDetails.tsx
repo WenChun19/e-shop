586e4d81281e9584456b2a5815ece9b0adb9bdf7
Hoh1996 <wenchun.sitegiant@gmail.com>
2023-11-29 21:05:05 +0800

    Add product image carousel

--- app/product/[productId]/ProductDetails.tsx
+++ app/product/[productId]/ProductDetails.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Button from "@/app/components/Button";
+import ProductImage from "@/app/components/products/ProductImage";
 import SetColor from "@/app/components/products/SetColor";
 import SetQuantity from "@/app/components/products/SetQuantity";
 import { Rating } from "@mui/material";
@@ -86,7 +87,11 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
       className="grid grid-cols-1 md:grid-cols-2
   gap-12"
     >
-      <div>Images</div>
+      <ProductImage
+        cartProduct={cartProduct}
+        product={product}
+        handleColorSelect={handleColorSelect}
+      />
       <div className="flex flex-col gap-1 text-slate-500 text-sm">
         <h2 className="text-3xl font-medium text-slate-700">{product.name}</h2>
         <div className="flex items-center gap-2">
